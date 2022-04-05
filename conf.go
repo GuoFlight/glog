fff77b63f7e5ec13e13c50a777660a7a8aea26ab
GuoFlight <1754811791@qq.com>
2022-04-05 18:33:37 +0800

    支持通过配置文件创建Logger，进而自定义是否输出Caller等信息

---
+++ conf.go
@@ -0,0 +1,31 @@
+package glog
+
+import "github.com/sirupsen/logrus"
+
+type Conf struct {
+	LogDir            string
+	LogLevel          LogLevel
+	LogFileCount      uint
+	IsStdoutPrint     bool //是否将日志输出到标准输出
+	CallerPrintLevels []LogLevel
+}
+
+type LogLevel logrus.Level
+
+const (
+	PanicLevel LogLevel = LogLevel(logrus.PanicLevel)
+	FatalLevel LogLevel = LogLevel(logrus.FatalLevel)
+	ErrorLevel LogLevel = LogLevel(logrus.ErrorLevel)
+	WarnLevel  LogLevel = LogLevel(logrus.WarnLevel)
+	InfoLevel  LogLevel = LogLevel(logrus.InfoLevel)
+	DebugLevel LogLevel = LogLevel(logrus.DebugLevel)
+	TraceLevel LogLevel = LogLevel(logrus.TraceLevel)
+)
+
+var DefaultConf = Conf{
+	LogDir:            "./logs",
+	LogLevel:          InfoLevel,
+	LogFileCount:      24,
+	IsStdoutPrint:     false,
+	CallerPrintLevels: []LogLevel{PanicLevel, FatalLevel, ErrorLevel, WarnLevel},
+}
--- log.go
+++ log.go
@@ -11,21 +11,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func NewLogger(logDir,logLevel string,AllLevelReportCaller bool,logFileCount uint)(*logrus.Logger,error){
+func NewLogger(logDir, logLevel string, AllLevelReportCaller bool, logFileCount uint) (*logrus.Logger, error) {
 	Logger := logrus.New()
 
 	//设置日志级别
-	logLevelLogrus,err := logrus.ParseLevel(logLevel)
-	if err!=nil{
-		return nil,err
+	logLevelLogrus, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		return nil, err
 	}
 	Logger.SetLevel(logLevelLogrus)
 
 	//输出Caller
-	if AllLevelReportCaller{
+	if AllLevelReportCaller {
 		//所有日志等级输出Caller
 		Logger.SetReportCaller(true)
-	} else{
+	} else {
 		//指定日志等级输出Caller
 		var printFileAndNumHook PrintFileAndNumHook
 		Logger.AddHook(&printFileAndNumHook)
@@ -34,89 +34,154 @@ func NewLogger(logDir,logLevel string,AllLevelReportCaller bool,logFileCount uin
 	//取消日志标准输出(终端输出)
 	if logLevelLogrus != logrus.DebugLevel {
 		nullFile, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-		if err!=nil{
+		if err != nil {
 			return nil, err
 		}
 		Logger.SetOutput(nullFile) //将日志输出到此文件
 	}
 
 	//多文件输出+日志切割
-	logFileTrace,err := GetWriter(path.Join(logDir,"TRACE.log"),logFileCount)
-	if err!=nil{
+	logFileTrace, err := GetWriter(path.Join(logDir, "TRACE.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFileDebug,err := GetWriter(path.Join(logDir,"DEBUG.log"),logFileCount)
-	if err!=nil{
+	logFileDebug, err := GetWriter(path.Join(logDir, "DEBUG.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFileInfo,err := GetWriter(path.Join(logDir,"INFO.log"),logFileCount)
-	if err!=nil{
+	logFileInfo, err := GetWriter(path.Join(logDir, "INFO.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFileWarn,err := GetWriter(path.Join(logDir,"WARN.log"),logFileCount)
-	if err!=nil{
+	logFileWarn, err := GetWriter(path.Join(logDir, "WARN.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFileError,err := GetWriter(path.Join(logDir,"ERROR.log"),logFileCount)
-	if err!=nil{
+	logFileError, err := GetWriter(path.Join(logDir, "ERROR.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFileFatal,err := GetWriter(path.Join(logDir,"FATAL.log"),logFileCount)
-	if err!=nil{
+	logFileFatal, err := GetWriter(path.Join(logDir, "FATAL.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
-	logFilePanic,err := GetWriter(path.Join(logDir,"PANIC.log"),logFileCount)
-	if err!=nil{
+	logFilePanic, err := GetWriter(path.Join(logDir, "PANIC.log"), logFileCount)
+	if err != nil {
 		return nil, err
 	}
 	pathMap := lfshook.WriterMap{
 		logrus.TraceLevel: logFileTrace,
 		logrus.DebugLevel: logFileDebug,
 		logrus.InfoLevel:  logFileInfo,
-		logrus.WarnLevel: logFileWarn,
+		logrus.WarnLevel:  logFileWarn,
 		logrus.ErrorLevel: logFileError,
 		logrus.FatalLevel: logFileFatal,
 		logrus.PanicLevel: logFilePanic,
 	}
 	Logger.AddHook(lfshook.NewHook(
 		pathMap,
-		&logrus.TextFormatter{},		//普通文本模式
+		&logrus.TextFormatter{}, //普通文本模式
 	))
 
-	return Logger,nil
+	return Logger, nil
 }
+func NewLoggerWithConf(conf Conf) (*logrus.Logger, error) {
+	Logger := logrus.New()
 
+	//设置日志级别
+	Logger.SetLevel(logrus.Level(conf.LogLevel))
+
+	//指定日志等级输出Caller
+	var printFileAndNumHook PrintFileAndNumHook
+	var levels []logrus.Level
+	for _, v := range conf.CallerPrintLevels {
+		levels = append(levels, logrus.Level(v))
+	}
+	printFileAndNumHook.CallerPrintLevels = levels
+	Logger.AddHook(&printFileAndNumHook)
+
+	//取消日志标准输出(终端输出)
+	if !conf.IsStdoutPrint {
+		nullFile, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		if err != nil {
+			return nil, err
+		}
+		Logger.SetOutput(nullFile) //将日志输出到此文件
+	}
+
+	//多文件输出+日志切割
+	logFileTrace, err := GetWriter(path.Join(conf.LogDir, "TRACE.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFileDebug, err := GetWriter(path.Join(conf.LogDir, "DEBUG.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFileInfo, err := GetWriter(path.Join(conf.LogDir, "INFO.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFileWarn, err := GetWriter(path.Join(conf.LogDir, "WARN.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFileError, err := GetWriter(path.Join(conf.LogDir, "ERROR.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFileFatal, err := GetWriter(path.Join(conf.LogDir, "FATAL.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	logFilePanic, err := GetWriter(path.Join(conf.LogDir, "PANIC.log"), conf.LogFileCount)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := lfshook.WriterMap{
+		logrus.TraceLevel: logFileTrace,
+		logrus.DebugLevel: logFileDebug,
+		logrus.InfoLevel:  logFileInfo,
+		logrus.WarnLevel:  logFileWarn,
+		logrus.ErrorLevel: logFileError,
+		logrus.FatalLevel: logFileFatal,
+		logrus.PanicLevel: logFilePanic,
+	}
+	Logger.AddHook(lfshook.NewHook(
+		pathMap,
+		&logrus.TextFormatter{}, //普通文本模式
+	))
+	return Logger, nil
+}
 
 //指定日志等级输出文件名+行号的hook
 type PrintFileAndNumHook struct {
+	CallerPrintLevels []logrus.Level
 }
+
 func (hook *PrintFileAndNumHook) Fire(entry *logrus.Entry) error {
-	pcs := make([]uintptr,10)			//容量要足够，否则会被截断
-	i := runtime.Callers(0,pcs)
+	pcs := make([]uintptr, 10)
+	i := runtime.Callers(0, pcs)
 	entry.Data["func"] = runtime.FuncForPC(pcs[i-2]).Name()
-	file,line := runtime.FuncForPC(pcs[i-2]).FileLine(pcs[i-2])
+	file, line := runtime.FuncForPC(pcs[i-2]).FileLine(pcs[i-2])
 	entry.Data["filename"] = file
 	entry.Data["lineNum"] = line
 	return nil
 }
 func (hook *PrintFileAndNumHook) Levels() []logrus.Level {
-	return []logrus.Level{logrus.PanicLevel, logrus.FatalLevel, logrus.ErrorLevel, logrus.WarnLevel} //在这些Levels上生效
+	return hook.CallerPrintLevels //在这些Levels上生效
 }
 
 //得到日志切割的输出对象
-func GetWriter(pathLogFile string,logFileCount uint) (*rotatelogs.RotateLogs,error){
+func GetWriter(pathLogFile string, logFileCount uint) (*rotatelogs.RotateLogs, error) {
 	writer, err := rotatelogs.New(
-		pathLogFile+".%Y%m%d%H",		        //日志文件后缀：年月日时
-		rotatelogs.WithLinkName(pathLogFile),	//为当前正在输出的日志文件建立软连接
-		rotatelogs.WithRotationCount(logFileCount),//日志文件保存的个数(包括当前正在输出的日志)
-		rotatelogs.WithRotationTime(time.Duration(1)*time.Hour),		//设置日志分割的时间(隔多久分割一次)
+		pathLogFile+".%Y%m%d%H",                                 //日志文件后缀：年月日时
+		rotatelogs.WithLinkName(pathLogFile),                    //为当前正在输出的日志文件建立软连接
+		rotatelogs.WithRotationCount(logFileCount),              //日志文件保存的个数(包括当前正在输出的日志)
+		rotatelogs.WithRotationTime(time.Duration(1)*time.Hour), //设置日志分割的时间(隔多久分割一次)
 	)
-	if err!=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
-	return writer,nil
+	return writer, nil
 }
-
-
-
-
